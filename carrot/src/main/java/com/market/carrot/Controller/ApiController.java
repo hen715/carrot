133c42d6506b030faca4d44fcf84c506a6f723d3
hen715 <hen715@naver.com>
2023-08-18 17:49:01 +0900

    api 저장 기능 추가

--- carrot/src/main/java/com/market/carrot/Controller/ApiController.java
+++ carrot/src/main/java/com/market/carrot/Controller/ApiController.java
@@ -1,9 +1,22 @@
 package com.market.carrot.Controller;
 
+import com.market.carrot.dto.ItemSaveDto;
+import com.market.carrot.service.ItemsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class ApiController {
+
+    private final ItemsService itemsService;
+
+    @PostMapping("/api/v1/items")
+    public Long save(@RequestBody ItemSaveDto itemSaveDto){
+        return itemsService.save(itemSaveDto);
+    }
 
 
 }
--- carrot/src/main/java/com/market/carrot/Controller/WebController.java
+++ carrot/src/main/java/com/market/carrot/Controller/WebController.java
@@ -1,15 +1,20 @@
 package com.market.carrot.Controller;
 
+import com.market.carrot.service.ItemsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class WebController {
 
+    private final ItemsService itemsService;
     @GetMapping("/")
-    public String main(){
+    public String main(Model model)
+    {
+        model.addAttribute("items",itemsService.findAll());
         return "main";
     }
 
