a14c9b13e79434c485f63a498f0c6c7a1fec44fd
hen715 <hen715@naver.com>
2023-08-18 19:24:29 +0900

    상품 찾기, 수정, 삭제 추가

--- carrot/src/main/java/com/market/carrot/service/ItemsService.java
+++ carrot/src/main/java/com/market/carrot/service/ItemsService.java
@@ -1,13 +1,17 @@
 package com.market.carrot.service;
 
+import com.market.carrot.domain.Item;
 import com.market.carrot.domain.ItemRepository;
+import com.market.carrot.dto.ItemDto;
 import com.market.carrot.dto.ItemListDto;
 import com.market.carrot.dto.ItemSaveDto;
+import com.market.carrot.dto.ItemUpdateDto;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -17,13 +21,33 @@ public class ItemsService {
     private final ItemRepository itemRepository;
 
     @Transactional
-    public Long save(ItemSaveDto itemSaveDto){
+    public Long save(ItemSaveDto itemSaveDto) {
         return itemRepository.save(itemSaveDto.toEntity()).getId();
     }
 
     @Transactional
-    public List<ItemListDto> findAll(){
+    public List<ItemListDto> findAll() {
         return itemRepository.findAll().stream().map(ItemListDto::new).collect(Collectors.toList());
     }
 
+    @Transactional
+    public ItemDto findById(Long id) {
+        Item dto = itemRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 상품이 없습니다."));
+        return new ItemDto(dto);
+    }
+
+    @Transactional
+    public Long update(Long id,ItemUpdateDto updateDto) {
+        Item item = itemRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 상품이 없습니다."));
+        item.update(updateDto.getTitle(),updateDto.getDescription(),updateDto.getPlace(),updateDto.getPrice(),updateDto.getImage());
+
+        return id;
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Item item = itemRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 상품이 없습니다."));
+        itemRepository.delete(item);
+    }
 }
+
