3a8c077e64bce7573b397e5084732ea88dd5fc7d
hen715 <hen715@naver.com>
2023-08-21 14:53:30 +0900

    세션 추가

--- carrot/src/main/java/com/market/carrot/Controller/WebController.java
+++ carrot/src/main/java/com/market/carrot/Controller/WebController.java
@@ -1,17 +1,24 @@
 package com.market.carrot.Controller;
 
+import com.market.carrot.domain.user.User;
+import com.market.carrot.dto.UserDto;
 import com.market.carrot.service.ItemsService;
+import com.market.carrot.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.h2.engine.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RequiredArgsConstructor
 @Controller
 public class WebController {
 
     private final ItemsService itemsService;
+    private final UserService userService;
     @GetMapping("/")
     public String main(Model model)
     {
@@ -39,4 +46,16 @@ public class WebController {
     public String joinUser(){
         return "join";
     }
+
+    @PostMapping("/login")
+    public String login(@RequestParam("email") String email, @RequestParam("password") String password, HttpServletRequest request){
+        UserDto user = userService.login(email,password);
+        if(user == null){
+            return "main";
+        }
+        HttpSession session = request.getSession();
+        session.setAttribute("loginUser",user);
+        return "main";
+    }
+
 }
--- carrot/src/main/java/com/market/carrot/service/UserService.java
+++ carrot/src/main/java/com/market/carrot/service/UserService.java
@@ -1,6 +1,8 @@
 package com.market.carrot.service;
 
+import com.market.carrot.domain.user.User;
 import com.market.carrot.domain.user.UserRepository;
+import com.market.carrot.dto.UserDto;
 import com.market.carrot.dto.UserSaveDto;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -15,5 +17,20 @@ public class UserService {
     @Transactional
     public Long join(UserSaveDto userSaveDto){
         return userRepository.save(userRepository.save(userSaveDto.toEntity())).getId();
+    }
+
+    @Transactional
+    public UserDto login(String email, String password){
+        UserDto user = findByEmail(email);
+        if(user.getPassword().equals(password))
+            return user;
+        else return null;
+
+    }
+
+    @Transactional
+    public UserDto findByEmail(String email){
+        User user = userRepository.findByEmail(email).orElseThrow(()->new IllegalArgumentException("존재하지 않는 이메일입니다."));
+        return new UserDto(user);
     }
 }
